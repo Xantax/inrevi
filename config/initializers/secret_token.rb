4d825d4b5b635747e9beb1fa712265f270da2746
Xantax <xantar@ymail.com>
2014-01-16 22:14:35 +0000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Inrevi::Application.config.secret_key_base = 'c3e661df4e57a69b89f2cad5f733acc139564bd69b0163d2aa8f7f55175b77cb2b80f62fbd384a4426ae4fcdc5f2f3e038f24b7e96ca913f619680198261f460'
