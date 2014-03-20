3eefb8f9b5d630c55953c8750bdf82ce39e5d2f9
Xantax <xantar@ymail.com>
2014-03-20 12:56:14 +0000

    TV shows index, search and show basics

--- app/controllers/tvshows_controller.rb
+++ app/controllers/tvshows_controller.rb
@@ -1,7 +1,23 @@
 class TvshowsController < ApplicationController
+  require 'themoviedb'
+
+  before_filter :set_config
+  Tmdb::Api.key("a8ddb278788ceab2a58875b7f172c327")
+
+  def set_config
+  	@configuration = Tmdb::Configuration.new
+  end
+    
   def index
+    @popular = Tmdb::TV.popular
   end
 
   def show
+    @tvshow = Tmdb::TV.detail(params[:id])
   end
+  
+  def search
+    @tvshow = Tmdb::TV.find(params[:query])
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140319112845) do
+ActiveRecord::Schema.define(version: 20140320111138) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -210,6 +210,11 @@ ActiveRecord::Schema.define(version: 20140319112845) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "image"
+  end
+
+  create_table "tvshows", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
