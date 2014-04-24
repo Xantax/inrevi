f0a83baa860ff9ab1efb77f631d0a06c920b86a4
Xantax <xantar@ymail.com>
2014-04-24 19:01:42 +0000

    remove old book data(openlibrary) plus some changes

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,38 +1,3 @@
 module BooksHelper
   
-  def cover_picture book
-    if book && book.edition_key
-      edition_key = book.edition_key.first
-      image_url = "https://covers.openlibrary.org/w/olid/#{edition_key}-S.jpg"
-      return image_url if exist_image_with_edition_key(edition_key)
-    end
-    '/NoImage.png'
-  end
-
-  def exist_image_with_edition_key edition_key
-    size = FastImage.size "https://covers.openlibrary.org/w/olid/#{edition_key}-S.jpg"
-    size && size[0] > 1 && size[1] > 1
-  end
-
-  def cover cover
-    cover['medium'] if cover
-  end
-
-  def published_place places
-    address = []
-    if places
-      address = places.inject([]) { |address, place| address << place['name'] }
-    end
-
-    address.join(', ')
-  end
-
-  def subjects subjects
-    subjects.map { |subject| subject['name'] }.join(', ') if subjects
-  end
-
-  def publishers publishers
-    publishers.map { |publisher| publisher['name'] }.join(', ') if publishers
-  end
-  
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,28 +1,3 @@
 class Book < ActiveRecord::Base
   
-  CATEGORY = {title: "Name", author: "Author", subject: "Subject"}.freeze
-  PER_PAGE = 10
-
-  class << self
-    def search params
-      client = Openlibrary::Client.new
-      begin
-      client.search search_params(params), page(params), PER_PAGE
-      rescue Exception
-        nil
-      end
-    end
-    
-    def page params
-      params.has_key?(:page) ? params[:page].to_i : 1
-    end
-
-    def search_params params
-      if params[:category].present? && CATEGORY.include?(params[:category].to_sym)
-        {params[:category].to_sym => params[:query]}
-      else
-        params[:query]
-      end
-    end
-  end
 end
