fd367e2e959e6fd3d6925187eae8c93b80c4a71d
Xantax <xantar@ymail.com>
2014-05-08 01:48:48 +0000

    modify permissions and points to fit new review_controllers

--- app/models/merit/point_rules.rb
+++ app/models/merit/point_rules.rb
@@ -1,42 +1,57 @@
-# Be sure to restart your server when you modify this file.
-#
-# Points are a simple integer value which are given to "meritable" resources
-# according to rules in +app/models/merit/point_rules.rb+. They are given on
-# actions-triggered, either to the action user or to the method (or array of
-# methods) defined in the +:to+ option.
-#
-# 'score' method may accept a block which evaluates to boolean
-# (recieves the object as parameter)
-
 module Merit
   class PointRules
     include Merit::PointRulesMethods
 
     def initialize
-      # score 10, :on => 'users#update' do
-      #   user.name.present?
-      # end
-      #
+
       score 5, :on => ['auto_reviews#create',
-        'local_reviews#create'
+        'local_reviews#create',
+        'podcast_reviews#create',
+        'tech_reviews#create',
+        'movie_reviews#create',
+        'tvshow_reviews#create',
+        'song_reviews#create',
+        'book_reviews#create'
+        
         ], category: 'gold_points'
+      
       
       score 2, :on => ['auto_reviews#upvote',
-        'local_reviews#upvote'
+        'local_reviews#upvote',
+        'podcast_reviews#upvote',
+        'tech_reviews#upvote',
+        'movie_reviews#upvote',
+        'tvshow_reviews#upvote',
+        'song_reviews#upvote',
+        'book_reviews#upvote',
+        
         ], to: :user, category: 'gold_points'
       
+      
       score 1, :on => ['auto_reviews#upvote', 'auto_reviews#downvote',
-        'local_reviews#upvote', 'local_reviews#downvote'
+        'local_reviews#upvote', 'local_reviews#downvote',
+        'tech_reviews#upvote', 'tech_reviews#downvote',
+        'podcast_reviews#upvote', 'podcast_reviews#downvote',
+        'movie_reviews#upvote', 'movie_reviews#downvote',
+        'tvshow_reviews#upvote', 'tvshow_reviews#downvote',
+        'song_reviews#upvote', 'song_reviews#downvote',
+        'book_reviews#upvote', 'book_reviews#downvote',
+        
         ], category: 'gold_points'
       
+      
+      
       score 1, :on => ['auto_reviews#create',
-        'local_reviews#create'
+        'local_reviews#create',
+        'podcast_reviews#create',
+        'tech_reviews#create',
+        'movie_reviews#create',
+        'tvshow_reviews#create',
+        'song_reviews#create',
+        'book_reviews#create'
+        
         ], to: :user, category: 'just_reviews'
-      #
-      # score 20, :on => [
-      #   'comments#create',
-      #   'photos#create'
-      # ]
+
     end
   end
 end
--- app/models/permission.rb
+++ app/models/permission.rb
@@ -3,22 +3,35 @@ class Permission < Struct.new(:user)
     return true if controller == "sessions"
     return true if controller == "static_pages" && action != "admin_dashboard"
     return true if controller == "books"
+    return true if controller == "songs"
     return true if controller == "locals"
     return true if controller == "autos" && action.in?(%w[index show])
     return true if controller == "users" && action.in?(%w[show])
     return true if controller == "contact_forms" && action.in?(%w[new create])
     return true if controller == "auto_reviews" && action.in?(%w[index show])
+    return true if controller == "podcast_reviews" && action.in?(%w[index show])
+    return true if controller == "tech_reviews" && action.in?(%w[index show])
+    return true if controller == "movie_reviews" && action.in?(%w[index show])
+    return true if controller == "tvshow_reviews" && action.in?(%w[index show])
+    return true if controller == "song_reviews" && action.in?(%w[index show])
+    return true if controller == "local_reviews" && action.in?(%w[index show])
+    return true if controller == "book_reviews" && action.in?(%w[index show])
     if user
       return true if user.admin?
       #return false if user.banned?
       if user.moderator?
-        return true if controller == "auto_reviews" && action.in?(%w[edit update all])
+        return true if controller == "users" && action.in?(%w[edit update])
+        return true if controller == "auto_reviews" && action.in?(%w[edit update all])        
       end
       return true if controller == "relationships" && action.in?(%w[create destroy])
       return true if controller == "auto_reviews" && action.in?(%w[new create upvote downvote])
       return true if controller == "local_reviews" && action.in?(%w[new create upvote downvote])
-      return true if controller == "locals" && action.in?(%w[index])
-      return true if controller == "users" && action.in?(%w[edit update])
+      return true if controller == "song_reviews" && action.in?(%w[new create upvote downvote])
+      return true if controller == "tech_reviews" && action.in?(%w[new create upvote downvote])
+      return true if controller == "movie_reviews" && action.in?(%w[new create upvote downvote])
+      return true if controller == "tvshow_reviews" && action.in?(%w[new create upvote downvote])
+      return true if controller == "podcast_reviews" && action.in?(%w[new create upvote downvote])
+      return true if controller == "book_reviews" && action.in?(%w[new create upvote downvote])
     end
     false
   end
