a43532efa4db04c9f2fcf1dfa7cb44c788379cce
Xantax <xantar@ymail.com>
2014-05-30 20:42:23 +0000

    heroku dpl 8

--- public/assets/reviews_icon-944f9ca514208ff6c87e88161ce24f84.js
+++
@@ -1,7 +0,0 @@
-$(document).on('click', '.thumbsdown a, .thumbsup a', function()
-{
-    var parent = $(this).closest('.separatereview');
-    $('.thumbsdown', parent).before('Voted!');
-    $('.thumbsdown, .thumbsup', parent).remove();
-})
-;
