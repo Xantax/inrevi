c32639203b262d3399c074871d7b85f7fa4f361f
Xantax <xantar@ymail.com>
2014-05-31 15:19:19 +0000

    heroku dpl 17 omna

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,11 +1,11 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, 'ENV["FB_KEY"]', 'ENV["FB_SECRET"]',{
+  provider :facebook, ENV["FB_KEY"], ENV["FB_SECRET"],{
     :image_size => 'large'
     }
-  provider :google_oauth2, 'ENV["GPLUS_KEY"]', 'ENV["GPLUS_SECRET"]',{
+  provider :google_oauth2, ENV["GPLUS_KEY"], ENV["GPLUS_SECRET"],{
       :image_aspect_ratio => "original",
-      :image_size => 200
+    :image_size => 200
     }
 end
