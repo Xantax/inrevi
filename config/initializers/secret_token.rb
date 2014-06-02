61bd2f6d7066b08cd2aaffa75590a7d57741fdae
Xantax <xantar@ymail.com>
2014-06-02 17:08:02 +0000

    heroku dpl test 21

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Inrevi::Application.config.secret_key_base = 'c3e661df4e57a69b89f2cad5f733acc139564bd69b0163d2aa8f7f55175b77cb2b80f62fbd384a4426ae4fcdc5f2f3e038f24b7e96ca913f619680198261f460'
+Inrevi::Application.config.secret_key_base = ENV['SECRET_TOKEN']
