51c51f6bb15174e958244f12c089544d5b0b0c2d
Xantax <xantar@ymail.com>
2014-06-20 23:20:40 +0000

    assets precompile 11

--- public/assets/reviews_icon-09307c6275d6905bb3b23b3ca83fd5d6.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  $(document).on("click", ".thumbsdown a, .thumbsup a", function() {
-    var parent;
-    parent = $(this).closest(".separatereview");
-    $(".thumbsdown", parent).before("Voted!");
-    $(".thumbsdown, .thumbsup", parent).remove();
-  });
-
-}).call(this);
