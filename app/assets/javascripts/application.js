c78b2e4aa62e32378a4cd3d1c328c0f8e397d35b
Xantax <xantar@ymail.com>
2014-06-20 20:53:11 +0000

    assets precompile 4

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,6 +9,5 @@
 //
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
-//= require jquery.turbolinks
-//= require products
+//
 //= require_tree .
