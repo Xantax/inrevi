fad60d6f72081722c63db25638b434a72d42c3a3
Xantax <xantar@ymail.com>
2014-06-20 21:02:48 +0000

    assets precompile 5

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,8 @@ module Inrevi
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.serve_static_assets = true
-    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif vendor.js vendor.css application.js application.css)
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
+    config.assets.precompile += ['application.js', 'application.css', 'vendor.js', 'vendor.css']
     config.autoload_paths += %W(#{config.root}/lib #{config.root}/app/uploaders)
     config.autoload_paths += %W(#{config.root}/queries)
     
