babbc9bd5d6ca9aceb8645c2337eaef60fa821d0
Xantax <xantar@ymail.com>
2014-06-21 23:45:35 +0000

    remove sign in notice

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -2,7 +2,6 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def all
     user = User.from_omniauth(request.env["omniauth.auth"])
     if user.persisted?
-      store_location
       sign_in_and_redirect user
     else
       session["devise.user_attributes"] = user.attributes
