9a7dcdb5ab1a1093c4788bb3d1d32e96a158f808
Xantax <xantar@ymail.com>
2014-07-30 20:03:26 +0000

    fix migration problems

---
+++ db/migrate/20140501005741_create_promotions.rb
@@ -0,0 +1,21 @@
+class CreatePromotions < ActiveRecord::Migration
+  def change
+    create_table :promotions do |t|
+      t.string :text
+      t.string :link
+      t.boolean  :local
+      t.boolean  :auto
+      t.boolean  :tech
+      t.boolean  :book
+      t.boolean  :movie
+      t.boolean  :music
+      t.boolean  :podcast
+      t.boolean  :tvshow
+      t.boolean  :drug
+      t.boolean  :recipe
+      t.boolean  :art
+
+      t.timestamps
+    end
+  end
+end
