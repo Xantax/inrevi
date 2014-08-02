76c0e0468b5238d346e63ce98958c5f1d43b8433
Xantax <xantar@ymail.com>
2014-08-02 20:31:18 +0000

    add personal subcategory

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -53,11 +53,11 @@ class StaticPagesController < ApplicationController
   private
   
   def set_activities
-     if signed_in?
-       @popular_users = User.popular_users.map(&:id)
-       @popular_activities = PublicActivity::Activity.order("created_at desc").where(owner_id: @popular_users, owner_type: "User").limit(15)
+    if signed_in?
+      @popular_users = User.popular_users.map(&:id)
+      @popular_activities = PublicActivity::Activity.order("created_at desc").where(owner_id: @popular_users, owner_type: "User").limit(10)
   
-       @activities = PublicActivity::Activity.paginate(:page => params[:page], :per_page => 15).order("created_at desc").where(owner_id: current_user.followed_user_ids, owner_type: "User")    
+      @activities = PublicActivity::Activity.paginate(:page => params[:page], :per_page => 15).order("created_at desc").where(owner_id: current_user.followed_user_ids, owner_type: "User")    
     end
   end
   
